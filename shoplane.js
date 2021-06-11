e7091b0240881751d354f02d8e6a8b790b99614f
sayali-ally <sayali@ally.io>
2021-06-11 23:26:35 +0530

    shoplane

---
+++ shoplane.js
@@ -0,0 +1,41 @@
+
+
+    const clothing = document.getElementById('clothing');
+    const accessories = document.getElementById('accessories');
+
+    for (let product of productList) {
+
+        const productCard = document.createElement('div');
+        productCard.classList.add('card');
+
+        const productImgContainer = document.createElement('div');
+        productImgContainer.classList.add('img');
+        const imgTag = document.createElement('img');
+        imgTag.src = product.preview;
+        productImgContainer.appendChild(imgTag);
+
+        const productDetails = document.createElement('div');
+        productDetails.classList.add('details');
+
+        const productName = document.createElement('h3');
+        productName.innerHTML = product.name;
+        const productBrand = document.createElement('h4');
+        productBrand.innerHTML = product.brand;
+        const productPrice = document.createElement('h5');
+        productPrice.innerHTML = "Rs "+ product.price;
+
+        productDetails.appendChild(productName);
+        productDetails.appendChild(productBrand);
+        productDetails.appendChild(productPrice);
+
+        productCard.appendChild(productImgContainer);
+        productCard.appendChild(productDetails);
+    }
+    for(let i=0;i<5;i++)
+    {
+        clothing.appendChild(productCard)
+    }
+    for(let i=0;i<5;i++)
+    {
+        accessories.appendChild(productCard)
+    }
